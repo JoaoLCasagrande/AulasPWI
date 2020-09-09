69d0a2d385e629017734339d2a0854d041713b63
JoaoLCasagrande <joaoluizcasagrande@gmail.com>
2020-09-09 15:16:11 -0300

    09/09/2020

---
+++ SQL/exer1_list2-banco.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE banco;
+USE banco;
+
+CREATE TABLE estados(
+    id_estado INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(191) NOT NULL,
+    sigla CHAR (2),
+    pais_id INT UNSIGNED NOT NULL
+);
+CREATE TABLE cidades(
+    id_cidade INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(191),
+    estado_id INT UNSIGNED NOT NULL
+);
+CREATE TABLE telefones(
+    id_telefone INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    numero VARCHAR(191) NOT NULL,
+    cliente_id INT UNSIGNED NOT NULL
+);
+CREATE TABLE agencias (
+    id_agencia INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    numero_agencia INT UNSIGNED NOT NULL,
+    nome VARCHAR(191) NOT NULL,
+    endereco VARCHAR(191) NOT NULL,
+    cidade_id INT UNSIGNED NOT NULL
+);
+CREATE TABLE clientes (
+    id_cliente INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(191) NOT NULL,
+    cpf VARCHAR(191) NOT NULL,
+    endereco VARCHAR(191) NOT NULL,
+    cidade_id INT UNSIGNED NOT NULL
+);
+CREATE TABLE contas_correntes (
+    id_conta_corrente INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    numero_identificador INT UNSIGNED NOT NULL,
+    cliente_id INT UNSIGNED,
+    agencia_id INT UNSIGNED,
+    saldo DECIMAL NOT NULL
+);
+SHOW TABLES;
