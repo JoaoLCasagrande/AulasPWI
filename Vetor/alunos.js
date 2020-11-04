6d7ad93ecb2a5d09ecdc0951cce834edbdbe8862
JoaoLCasagrande <61015263+JoaoLCasagrande@users.noreply.github.com>
2020-11-04 16:44:17 -0300

    04/11/2020

---
+++ Vetor/alunos.js
@@ -0,0 +1,16 @@
+var alunos = [5];
+
+aluno[1] = "Natiara";
+
+aluno [1] = "João Pedro";
+
+aluno [2] = "Maria";
+
+aluno [3] = "Paulo";
+
+aluno [4] = "Monique";
+
+aluno [5] = "André";
+
+console.log(alunos);
+
